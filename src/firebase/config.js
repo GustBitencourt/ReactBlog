a350c9cbb001ba769f49253612f5ec22eebf4011
GustBitencourt <gubiten@gmail.com>
2022-06-10 14:06:09 -0300

    Configuracao firebase

---
+++ src/firebase/config.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBjrdiUyAnS-2OxVRmFhylLjA-DOkUlqRI",
+  authDomain: "blogreact-c36cf.firebaseapp.com",
+  projectId: "blogreact-c36cf",
+  storageBucket: "blogreact-c36cf.appspot.com",
+  messagingSenderId: "408219322057",
+  appId: "1:408219322057:web:c5450f96c546a1fe06af18"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
