c6e96f4b4216f60ec0966990b89f16d9880b674e
GustBitencourt <gubiten@gmail.com>
2022-06-14 09:48:35 -0300

    passando usuario no contexto

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,46 @@
+//bibliotecas
+import { useState, useEffect } from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { onAuthStateChanged } from "firebase/auth";
 
+//contexto
+import { AuthContextProvider } from "./context/authContext";
+
+//custom hooks
+import { useAuthentication } from './hooks/useAuthentication';
+
+//pages
 import { Home } from "./pages/Home";
 import { About } from "./pages/About";
 import { Login } from "./pages/Login";
 import { Cadastro } from "./pages/Cadastro";
 
+//components
 import { Header } from "./components/Header";
 import { Footer } from "./components/Footer";
 
 import "./App.css";
 
 function App() {
+  const [user, setUser] = useState(undefined);
+  const { auth } = useAuthentication();
+
+  const loadingUser = user === undefined;
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      setUser(user);
+    });
+
+  }, [auth]);
+
+  if(loadingUser) {
+    return <p>Carregando...</p>
+  }
+
   return (
-    <div className="App">
-    
-      <AuthContextProvider>
+    <div className="App">    
+      <AuthContextProvider value={user}>
         <BrowserRouter>
           <Header />
           <div className="container">
