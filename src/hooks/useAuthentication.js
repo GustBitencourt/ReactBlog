2ce99d085f93edc8a64628733b6dbbd6c18b9033
GustBitencourt <gubiten@gmail.com>
2022-06-15 14:10:05 -0300

    Estrutura e estilo de createPost, hook para insercao de post

--- src/hooks/useAuthentication.js
+++ src/hooks/useAuthentication.js
@@ -93,7 +93,6 @@ export const useAuthentication = () => {
 
       setError(systemErrorMessage);
       setLoading(false);
-
     }
   }
 
--- src/pages/CreatePost/index.jsx
+++ src/pages/CreatePost/index.jsx
@@ -1,9 +1,77 @@
-import styles from './styles.module.css'
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useAuthValue } from '../../context/AuthContext';
+
+import styles from './styles.module.css';
 
 export const CreatePost = () => {
+  const [title, setTitle] = useState('');
+  const [image, setImage] = useState('');
+  const [body, setBody] = useState('');
+  const [tags, setTags] = useState([]);
+  const [formError, setFormError] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  }
+
   return (
-    <div>
-        <h1>CreatePost</h1>
+    <div className={styles.create_post}>
+        <h2>Crie seu post</h2>
+        <p>Mostre o que quiser!</p>
+        <form onSubmit={handleSubmit}>
+          <label>
+            <span>Título:</span>
+            <input 
+              type="text"
+              name="title"
+              placeholder="Pense num bom título"
+              value={title}
+              onChange={(event) => setTitle(event.target.value)}
+              required
+            />
+          </label>
+
+          <label>
+            <span>Imagem:</span>
+            <input 
+              type="text"
+              name="image"
+              placeholder="Pense em uma Imagem que reflita seu post"
+              value={image}
+              onChange={(event) => setImage(event.target.value)}
+              required
+            />
+          </label>
+
+          <label>
+            <span>Conteúdo:</span>
+            <textarea
+              name="body"
+              required
+              value={body}
+              onChange={(event) => setBody(event.target.value)}
+              placeholder="Insira o conteúdo do post"
+            ></textarea>
+          </label>
+
+          <label>
+            <span>Tags:</span>
+            <input 
+              type="text"
+              name="image"
+              placeholder="Insira as tags separadas por vírgulas"
+              value={tags}
+              onChange={(event) => setTags(event.target.value)}
+              required
+            />
+          </label>
+          <button className='btn'>Postar</button>
+          {/* {!loading && <button className='btn'>Postar</button>}
+          {loading && <button className='btn' disabled>Aguarde... </button>}
+
+          {error && <p className='error'>{error}</p>} */}
+        </form>
     </div>
   )
 }
