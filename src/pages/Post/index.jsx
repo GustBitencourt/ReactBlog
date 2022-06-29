45757526d15d4733a6b2b9eedab53f05872b4329
GustBitencourt <gubiten@gmail.com>
2022-06-29 16:24:15 -0300

    estilo pagina post unico

--- src/pages/Post/index.jsx
+++ src/pages/Post/index.jsx
@@ -9,10 +9,22 @@ export const Post = () => {
     const { document: post, loading, error } = useFetchDocument("posts", id);
 
   return (
-    <div>
+    <div className={styles.post_container}>
         {loading && <p>Carregando...</p>}
         {post && (
-            <h1>{post.title}</h1>
+            <>
+                <h1>{post.title}</h1>
+                <img src={post.image} alt={post.title} />
+                <p>{post.body}</p>
+                <h3>Post sobre:</h3>
+                <div className={styles.tags}>
+                    {post.tagsArray.map((tag) => (
+                        <p key={tag}>
+                            <span>#</span>{tag}
+                        </p>
+                    ))}
+                </div>
+            </>
         )}
     </div>
   )
