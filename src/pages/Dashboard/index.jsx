33e922cf2191ce17fcad125b0ebfb443be3a0043
GustBitencourt <gubiten@gmail.com>
2022-06-30 10:41:42 -0300

    dashboar mensagem para quando nao tiver post do usuario

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -1,9 +1,32 @@
+import { Link } from 'react-router-dom'
+import { useAuthValue } from "../../context/AuthContext";
+import { useFetchDocuments } from "../../hooks/useFetchDocuments";
+
 import styles from './styles.module.css'
 
 export const Dashboard = () => {
+  const { user } = useAuthValue();
+  const uid = user.uid;
+
+  //posts do usuario
+  const posts = [];
+
   return (
     <div>
-        <h1>Dashboard</h1>
+        <h2>Dashboard</h2>
+        <p>Gerencie os seus posts</p>
+        {posts && posts.lenght === 0 ? (
+          <div>
+            <p>Nenhum post encontrado</p>
+            <Link to='/posts/create' className="btn" >Criar um post</Link>
+          </div>
+        ) : (
+          <div>
+            
+          </div>
+        )
+
+        }
     </div>
   )
 }
